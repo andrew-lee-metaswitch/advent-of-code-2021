be7580944602767ad02d9ef7ac4373296f334c2d
Andrew Lee <andrew.lee@metaswitch.com>
2021-12-02 10:44:24 +0000

    JPR Markups

--- src/day01.rs
+++ src/day01.rs
@@ -1,29 +1,30 @@
 use crate::util;
 
-fn part_one(sea_depth_measurements: &Vec<u16>) {
+fn part_one(sea_depth_measurements: &[u16]) {
     let mut times_it_gets_deeper: usize = 0;
 
     for (s_position, s) in sea_depth_measurements.iter().enumerate() {
-        if s_position < sea_depth_measurements.len() - 1 {
-            if sea_depth_measurements[s_position + 1] > *s {
-                times_it_gets_deeper += 1;
-            }
+        if s_position < sea_depth_measurements.len() - 1
+            && sea_depth_measurements[s_position + 1] > *s
+        {
+            times_it_gets_deeper += 1;
         }
     }
     println!("The times is gets deeper is {}", times_it_gets_deeper);
 }
 
-fn part_two(sea_depth_measurements: &Vec<u16>) {
+fn part_two(sea_depth_measurements: &[u16]) {
     // This really boils down to comapring the measurement 3 along from where you are.
     let mut times_it_gets_deeper: usize = 0;
 
     for (s_position, s) in sea_depth_measurements.iter().enumerate() {
-        if s_position < sea_depth_measurements.len() - 3 {
-            if sea_depth_measurements[s_position + 3] > *s {
-                times_it_gets_deeper += 1;
-            }
+        if s_position < sea_depth_measurements.len() - 3
+            && sea_depth_measurements[s_position + 3] > *s
+        {
+            times_it_gets_deeper += 1;
         }
     }
+
     println!("The times is gets deeper is {}", times_it_gets_deeper);
 }
 
