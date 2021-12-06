17eb1e1a79d5d98041d3ba7635db2efcc20b85dc
Andrew Lee <andrew.lee@metaswitch.com>
2021-12-06 10:23:53 +0000

    Day 06

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
-//mod day06;
+mod day06;
 //mod day07;
 //mod day08;
 //mod day09;
@@ -44,7 +44,7 @@ fn main() {
         3 => day03::day03(),
         4 => day04::day04(),
         5 => day05::day05(),
-        //6 => day06::day06(),
+        6 => day06::day06(),
         //7 => day07::day07(),
         //8 => day08::day08(),
         //9 => day09::day09(),
