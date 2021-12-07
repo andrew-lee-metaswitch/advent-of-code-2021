08b2e1dc8c70458b680e1f4950e511478a758361
Andrew Lee <andrew.lee@metaswitch.com>
2021-12-07 10:54:33 +0000

    More natural way to find min

--- src/day07.rs
+++ src/day07.rs
@@ -1,44 +1,39 @@
 use crate::util;
 
 fn part_one(crab_submarines: &[i32]) {
-    let mut min: Option<i32> = None;
-    for position in *crab_submarines.iter().min().unwrap()..*crab_submarines.iter().max().unwrap()+1 {
-        let fuel_needed = crab_submarines.iter().map(|v| (v-position).abs()).sum::<i32>();
-        match min {
-            Some(m) => {
-                if fuel_needed < m {
-                    min = Some(fuel_needed)
-                }
-            }
-            None => {
-                min = Some(fuel_needed)
-            }
-        }
+    let mut distances: Vec<i32> = [].to_vec();
+    for position in
+        *crab_submarines.iter().min().unwrap()..*crab_submarines.iter().max().unwrap() + 1
+    {
+        distances.push(
+            crab_submarines
+                .iter()
+                .map(|v| (v - position).abs())
+                .sum::<i32>(),
+        );
     }
     println!(
         "The minimum amount of fuel to align is {:?}",
-        min
+        distances.iter().min().unwrap()
     );
 }
 
 fn part_two(crab_submarines: &[i32]) {
-    let mut min: Option<i32> = None;
-    for position in *crab_submarines.iter().min().unwrap()..*crab_submarines.iter().max().unwrap()+1 {
-        let fuel_needed = crab_submarines.iter().map(|v| (v-position).abs()).map(|v| (v*(v+1))/2).sum::<i32>();
-        match min {
-            Some(m) => {
-                if fuel_needed < m {
-                    min = Some(fuel_needed)
-                }
-            }
-            None => {
-                min = Some(fuel_needed)
-            }
-        }
+    let mut distances: Vec<i32> = [].to_vec();
+    for position in
+        *crab_submarines.iter().min().unwrap()..*crab_submarines.iter().max().unwrap() + 1
+    {
+        distances.push(
+            crab_submarines
+                .iter()
+                .map(|v| (v - position).abs())
+                .map(|v| (v * (v + 1)) / 2)
+                .sum::<i32>(),
+        );
     }
     println!(
         "The minimum amount of fuel to align is {:?}",
-        min
+        distances.iter().min().unwrap()
     );
 }
 
