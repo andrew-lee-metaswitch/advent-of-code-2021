ea94a78b54f7808546ad898de756ea874c894d84
Andrew Lee <andrew.lee@metaswitch.com>
2021-12-16 09:21:51 +0000

    Minor markups

--- src/day15.rs
+++ src/day15.rs
@@ -1,19 +1,18 @@
 use crate::util;
 use std::collections::{HashMap, HashSet};
 
-fn orthogonal_points(point: &[usize; 2], risk_levels: &[Vec<u8>]) -> Vec<[usize; 2]> {
-    let [row, column] = point;
+fn orthogonal_points([row, column]: &[usize; 2], height: usize, width: usize) -> Vec<[usize; 2]> {
     let mut orthogonal_points: Vec<[usize; 2]> = [].to_vec();
     if row != &0 {
         orthogonal_points.push([(row - 1), *column])
     }
-    if row != &(risk_levels.len() - 1) {
+    if row != &(height - 1) {
         orthogonal_points.push([(row + 1), *column])
     }
     if column != &0 {
         orthogonal_points.push([*row, (column - 1)])
     }
-    if column != &(risk_levels[0].len() - 1) {
+    if column != &(width - 1) {
         orthogonal_points.push([*row, (column + 1)])
     }
     orthogonal_points
@@ -22,38 +21,38 @@ fn orthogonal_points(point: &[usize; 2], risk_levels: &[Vec<u8>]) -> Vec<[usize;
 fn find_safest_route(risk_levels: &[Vec<u8>]) -> u32 {
     // Find the cheapest way to get from top-left to bottom-right
 
+    //let mut cheapest_path_to_point: HashMap<[usize; 2], u32> = HashMap::new();
     let mut cheapest_path_to_point: HashMap<[usize; 2], u32> = HashMap::new();
     cheapest_path_to_point.insert([0, 0], 0);
-
-    let mut new_cheapest_path_to_point = cheapest_path_to_point.clone();
     let mut places_to_go: HashSet<[usize; 2]> = HashSet::new();
     places_to_go.insert([0, 0]);
+    let height = risk_levels.len();
+    let width = risk_levels[0].len();
 
     // Right we haven't been to all cells or the HashMap is still changing...
     while !places_to_go.is_empty() {
-        cheapest_path_to_point = new_cheapest_path_to_point.clone();
+        // cheapest_path_to_point = new_cheapest_path_to_point.clone();
         let mut new_places_to_go: HashSet<[usize; 2]> = HashSet::new();
 
+        let old_cheapest_path_to_point = cheapest_path_to_point.clone();
         for (point, cost) in places_to_go
             .iter()
-            .map(|p| (p, cheapest_path_to_point.get(p).unwrap()))
+            .map(|p| (p, old_cheapest_path_to_point.get(p).unwrap()))
         {
-            let orthogonal_points_to_p = orthogonal_points(point, risk_levels);
-
-            for q in orthogonal_points_to_p {
+            for q in orthogonal_points(point, height, width) {
                 let path_cost_to_q = cost + risk_levels[q[0]][q[1]] as u32;
-                if !new_cheapest_path_to_point.contains_key(&q)
-                    || &path_cost_to_q < new_cheapest_path_to_point.get(&q).unwrap()
+                if !cheapest_path_to_point.contains_key(&q)
+                    || &path_cost_to_q < cheapest_path_to_point.get(&q).unwrap()
                 {
                     new_places_to_go.insert(q);
-                    new_cheapest_path_to_point.insert(q, path_cost_to_q);
+                    cheapest_path_to_point.insert(q, path_cost_to_q);
                 }
             }
         }
         places_to_go = new_places_to_go;
     }
-    let bottom_right = [risk_levels.len() - 1, risk_levels[0].len() - 1];
-    *new_cheapest_path_to_point.get(&bottom_right).unwrap()
+    let bottom_right = [height - 1, width - 1];
+    *cheapest_path_to_point.get(&bottom_right).unwrap()
 }
 
 fn part_one(risk_levels: &[Vec<u8>]) {
